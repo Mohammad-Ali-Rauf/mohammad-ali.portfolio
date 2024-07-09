763873a3012b45f0f3f993260abb1679397c43da
Mohammad Ali <m.aliadnanrauf@gmail.com>
2024-07-09 23:05:02 +0500

    further improved

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -5,7 +5,6 @@ import { client } from '../../../app/lib/sanity';
 
 import Image from 'next/image';
 import { useCallback, useEffect, useState } from 'react';
-import { ClipLoader } from 'react-spinners';
 
 interface Data {
 	title: string;
@@ -41,7 +40,7 @@ const Projects = () => {
 			setData(newData);
 		} catch (err) {
 			setError(err);
-			console.log(error)
+			console.log(error);
 		} finally {
 			setLoading(false);
 		}
@@ -55,62 +54,80 @@ const Projects = () => {
 		<div className='divide-y divide-gray-200 dark:divide-gray-700'>
 			<div className='space-y-2 pt-6 pb-8 md:space-y-5'>
 				<h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'>
-					All Projects
+					My Projects
 				</h1>
 			</div>
 
-			<div className='grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-10 pt-8'>
-				{loading ? (
-					<ClipLoader color='#38A89D' size={200} />
-				) : (
-					data?.map((project) => (
-						<article
-							key={project._id}
-							className='overflow-hidden dark:border-zinc-600 rounded-lg border border-gray-100 bg-white shadow-lg dark:shadow-gray-700 dark:bg-black shadow-teal-100'
-						>
-							<div className='h-56 w-full relative'>
-								<Image
-									fill
-									src={project.imageUrl}
-									alt='Image of the Project'
-									className='h-full w-full object-cover'
-								/>
+			<div className='grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-10 pt-8 pb-8'>
+				{data?.map((project) => (
+					<article
+						key={project._id}
+						className='overflow-hidden dark:border-zinc-600 rounded-lg border border-gray-100 bg-white shadow-md dark:shadow-gray-700 dark:bg-black shadow-teal-100'
+					>
+						<div className='h-56 w-full relative'>
+							<Image
+								fill
+								src={project.imageUrl}
+								alt='Image of the Project'
+								className='h-full w-full object-cover'
+							/>
+						</div>
+
+						<div className='p-4 sm:p-6'>
+							<h3 className='text-lg font-medium text-gray-900 dark:text-white'>
+								{project.title}
+							</h3>
+
+							<div className='mb-2 mt-2'>
+								<div className='flex flex-wrap'>
+									{Array.isArray(project.skills) ? (
+										project.skills.map((skill, index) => (
+											<span
+												key={index}
+												className='bg-teal-500 text-white text-sm font-medium px-2.5 py-0.5 rounded mr-2 mb-2'
+											>
+												{skill}
+											</span>
+										))
+									) : (
+										<span className='bg-green-500 text-white text-sm font-medium px-2.5 py-0.5 rounded'>
+											{project.skills}
+										</span>
+									)}
+								</div>
 							</div>
 
-							<div className='p-4 sm:p-6'>
-								<h3 className='text-lg font-medium text-gray-900 dark:text-white'>
-									{project.title}
-								</h3>
+							<p className='line-clamp-3 mt-2 text-sm leading-relaxed text-gray-500 dark:text-gray-400'>
+								{project.overview}
+							</p>
 
-								<div className='mb-2 mt-2'>
-									<div className='flex flex-wrap'>
-										{Array.isArray(project.skills) ? (
-											project.skills.map((skill, index) => (
-												<span
-													key={index}
-													className='bg-teal-500 text-white text-sm font-medium px-2.5 py-0.5 rounded mr-2 mb-2'
-												>
-													{skill}
-												</span>
-											))
-										) : (
-											<span className='bg-green-500 text-white text-sm font-medium px-2.5 py-0.5 rounded'>
-												{project.skills}
-											</span>
-										)}
-									</div>
-								</div>
-
-								<p className='line-clamp-3 mt-2 text-sm leading-relaxed text-gray-500 dark:text-gray-400'>
-									{project.overview}
-								</p>
-
-								<div className='pt-4 sm:pt-6 flex gap-6'>
+							<div className='pt-4 sm:pt-6 flex gap-6'>
+								<a
+									href={project.githubLink}
+									className='inline-flex p-3 text-lg rounded-lg dark:bg-teal-500 bg-gray-700 text-white'
+								>
+									<span className='pr-2'>Code</span>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										fill='none'
+										viewBox='0 0 24 24'
+										strokeWidth={1.5}
+										stroke='currentColor'
+										className='w-6 h-6'
+									>
+										<path
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											d='M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5'
+										/>
+									</svg>
+								</a>
+								{project.demoLink ? (
 									<a
-										href={project.githubLink}
+										href={project.demoLink}
 										className='inline-flex p-3 text-lg rounded-lg dark:bg-teal-500 bg-gray-700 text-white'
 									>
-										<span className='pr-2'>Code</span>
+										<span className='pr-2'>Live</span>
 										<svg
 											xmlns='http://www.w3.org/2000/svg'
 											fill='none'
@@ -122,41 +139,19 @@ const Projects = () => {
 											<path
 												strokeLinecap='round'
 												strokeLinejoin='round'
-												d='M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5'
+												d='M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418'
 											/>
 										</svg>
 									</a>
-									{project.demoLink ? (
-										<a
-											href={project.demoLink}
-											className='inline-flex p-3 text-lg rounded-lg dark:bg-teal-500 bg-gray-700 text-white'
-										>
-											<span className='pr-2'>Live</span>
-											<svg
-												xmlns='http://www.w3.org/2000/svg'
-												fill='none'
-												viewBox='0 0 24 24'
-												strokeWidth={1.5}
-												stroke='currentColor'
-												className='w-6 h-6'
-											>
-												<path
-													strokeLinecap='round'
-													strokeLinejoin='round'
-													d='M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418'
-												/>
-											</svg>
-										</a>
-									) : (
-										<span className='inline-flex p-3 text-lg rounded-lg dark:text-white dark:bg-red-500 bg-red-300 text-red-800'>
-											<span className='pr-2'>Not Available</span>
-										</span>
-									)}
-								</div>
+								) : (
+									<span className='inline-flex p-3 text-lg rounded-lg dark:text-white dark:bg-red-500 bg-red-300 text-red-800'>
+										<span className='pr-2'>Not Available</span>
+									</span>
+								)}
 							</div>
-						</article>
-					))
-				)}
+						</div>
+					</article>
+				))}
 			</div>
 		</div>
 	);
