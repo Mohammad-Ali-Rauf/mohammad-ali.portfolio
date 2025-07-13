4fc186fc51bc231822b8d1b9435b45062100028c
Mohammad Ali <m.aliadnanrauf@gmail.com>
2025-07-13 17:59:08 +0500

    changed to red teaming

---
+++ sanity/schemas/report.ts
@@ -0,0 +1,23 @@
+export default {
+    name: 'report',
+    title: 'Report',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'skills',
+            title: 'Skills Used',
+            type: 'array',
+            of: [{ type: 'string' }]
+        },
+        {
+            name: 'content',
+            title: 'Content',
+            type: 'string',
+        }
+    ],
+}
